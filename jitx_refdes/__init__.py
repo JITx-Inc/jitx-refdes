dd90b56497979728cd4d74a44ac6be1b636fa537
Checkpointer <checkpointer@noreply>
2026-05-21 21:38:32 +0000

    checkpoint:session-4e10a116-c74e-4acc-9419-ec7a4ece5668-turn-2ee58e7b-c15f-47e3-8dc5-915d4407a555-end head 72c65dd2d4cc4468d8b4a2f93e8654af342c9063 index-tree fc6e7788134b865c8e3a69d052134c6631e981fc worktree-tree ddf45343137f3b96ba4d334cff0ba80ccc995977 created 2026-05-21T21:38:32Z

---
+++ jitx_refdes/__init__.py
@@ -0,0 +1,14 @@
+from jitx_refdes.refdes import (
+    TableUpdateResult,
+    build_mapping,
+    renumber,
+    update_reference_designators_table,
+)
+
+__version__ = "0.1.0"
+__all__ = [
+    "TableUpdateResult",
+    "build_mapping",
+    "renumber",
+    "update_reference_designators_table",
+]
